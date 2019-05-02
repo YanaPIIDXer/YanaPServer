2ceece1b0c3c3868e4c6afd0cf1a5a228545147c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-05-02 13:04:07 +0900

    アプリケーションの根幹部分となる基底クラスを定義。

---
+++ YanaPServer/YanaPServer/Application/ApplicationBase.cpp
@@ -0,0 +1,57 @@
+#include "ApplicationBase.h"
+#include "YanaPServer/Socket/Listen/ListenSocket.h"
+using namespace YanaPServer::Socket::Listen;
+
+namespace YanaPServer
+{
+namespace Application
+{
+
+// コンストラクタ
+CApplicationBase::CApplicationBase(unsigned int ListenPort, const std::function<void(PeerPtr)> &OnConnectFunction)
+	: OnConnect(OnConnectFunction)
+{
+	CListenSocket::Get().Listen(ListenPort,
+		std::bind(&CApplicationBase::OnListen, this, std::placeholders::_1));
+}
+
+// デストラクタ
+CApplicationBase::~CApplicationBase()
+{
+	Peers.clear();
+}
+
+// 毎フレームの処理.
+void CApplicationBase::Poll()
+{
+	CListenSocket::Get().Poll();
+
+	PeerList::iterator It = Peers.begin();
+	while (It != Peers.end())
+	{
+		if (!(*It)->IsValid())
+		{
+			It = Peers.erase(It);
+		}
+		else
+		{
+			++It;
+		}
+	}
+}
+
+
+// Listenした。
+void CApplicationBase::OnListen(ISocket *pSocket)
+{
+	CPeerBase *pPeer = CreatePeer(pSocket);
+	if (pPeer == nullptr) { return; }
+
+	PeerSharedPtr pSharedPeer = PeerSharedPtr(pPeer);
+	Peers.push_back(pSharedPeer);
+
+	OnConnect(pSharedPeer);
+}
+
+}
+}
---
+++ YanaPServer/YanaPServer/Application/ApplicationBase.h
@@ -0,0 +1,77 @@
+#ifndef __APPLICATIONBASE_H__
+#define __APPLICATIONBASE_H__
+#include "YanaPServer/Socket/Socket.h"
+using namespace YanaPServer::Socket;
+#include "YanaPServer/Peer/PeerBase.h"
+using namespace YanaPServer::Peer;
+#include <vector>
+#include <memory>
+#include <functional>
+
+typedef std::weak_ptr<CPeerBase> PeerPtr;
+
+namespace YanaPServer
+{
+namespace Application
+{
+
+/**
+ * @class CApplicationBase
+ * @brief アプリケーションメイン基底クラス
+ */
+class CApplicationBase
+{
+
+private:		// 別名定義.
+
+	typedef std::shared_ptr<CPeerBase> PeerSharedPtr;
+	typedef std::vector<PeerSharedPtr> PeerList;
+
+public:
+
+	/**
+	 * @brief コンストラクタ
+	 * @param[in] ListenPort 待機ポート
+	 */
+	CApplicationBase(unsigned int ListenPort, const std::function<void(PeerPtr)> &OnConnectFunction);
+
+	/**
+	 * @brief デストラクタ
+	 */
+	virtual ~CApplicationBase() = 0;
+
+	/**
+	 * @fn void Poll()
+	 * @brief 毎フレームの処理
+	 * @detail メインフレームで呼び出す事。
+	 */
+	void Poll();
+
+protected:
+
+	/**
+	 * @fn virtual CPeerBase *CreatePeer(ISocket *pSocket) = 0
+	 * @brief Peer生成
+	 * @param[in] pSocket ソケット
+	 * @return CPeerBaseを派生したオブジェクトを生成して返す。
+	 */
+	virtual CPeerBase *CreatePeer(ISocket *pSocket) = 0;
+
+private:
+
+	// Peerリスト
+	PeerList Peers;
+
+	// 接続時コールバック
+	std::function<void(PeerPtr)> OnConnect;
+
+
+	// Listenした。
+	void OnListen(ISocket *pSocket);
+
+};
+
+}
+}
+
+#endif		// #ifndef __APPLICATIONBASE_H__
