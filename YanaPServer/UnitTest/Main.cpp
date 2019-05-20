76f19d241abdd30646046687217478b572c18ef6
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-05-20 22:54:25 +0900

    ・読み込み用ストリームクラス追加。 ・色々な型のシリアライズ関数追加。

--- YanaPServer/UnitTest/Main.cpp
+++ YanaPServer/UnitTest/Main.cpp
@@ -3,14 +3,57 @@
 // MemoryStream
 #include "YanaPServer/Util/Stream/MemorySizeCaliculator.h"
 #include "YanaPServer/Util/Stream/MemoryStreamWriter.h"
+#include "YanaPServer/Util/Stream/MemoryStreamReader.h"
 using namespace YanaPServer::Util::Stream;
+
+// シリアライズ
+bool Serialize(IMemoryStream *pStream, int *pTest1, short *pTest2, std::string *pTest3)
+{
+	if (!pStream->Serialize(pTest1)) { return false; }
+	if (!pStream->Serialize(pTest2)) { return false; }
+	if (!pStream->Serialize(pTest3)) { return false; }
+	return true;
+}
 
 // MemoryStreamのテスト
 bool TestMemoryStream()
 {
+	int Test1 = 123;
+	short Test2 = 456;
+	std::string Test3 = "Hoge";
+		
 	CMemorySizeCaliculator SizeCaliculator;
+	if (!Serialize(&SizeCaliculator, &Test1, &Test2, &Test3))
+	{
+		std::cout << "SizeCaliculator Serialize Failed." << std::endl;
+		return false;
+	}
 
 	CMemoryStreamWriter StreamWriter(SizeCaliculator.GetSize());
+	if (!Serialize(&StreamWriter, &Test1, &Test2, &Test3))
+	{
+		std::cout << "StreamWriter Serialize Failed." << std::endl;
+		return false;
+	}
+
+	int Test4 = 0;
+	short Test5 = 0;
+	std::string Test6 = "";
+	CMemoryStreamReader StreamReader(StreamWriter.GetBuffer(), StreamWriter.GetSize());
+	if (!Serialize(&StreamReader, &Test4, &Test5, &Test6))
+	{
+		std::cout << "StreamReader Serialize Failed." << std::endl;
+		return false;
+	}
+
+	if (Test4 != Test1 || Test5 != Test2 || Test6 != Test3)
+	{
+		std::cout << "Serialized Data Not Matched." << std::endl;
+		std::cout << "Test4:" << Test4 << std::endl;
+		std::cout << "Test5:" << Test5 << std::endl;
+		std::cout << "Test6:" << Test6 << std::endl;
+		return false;
+	}
 
 	return true;
 }
@@ -25,6 +68,7 @@ int main()
 		getchar();
 		return 1;
 	}
+	std::cout << "TestMemoryStream OK." << std::endl;
 
 	getchar();
 	return 0;
--- YanaPServer/YanaPServer/src/Util/Stream/MemoryStreamWriter.cpp
+++ YanaPServer/YanaPServer/src/Util/Stream/MemoryStreamWriter.cpp
@@ -27,49 +27,61 @@ CMemoryStreamWriter::~CMemoryStreamWriter()
 }
 
 // intのシリアライズ
-bool CMemoryStreamWriter::Serialize(const int *pData)
+bool CMemoryStreamWriter::Serialize(int *pData)
 {
 	int Data = CEndianConverter::Convert(*pData);
 	return Write(&Data, sizeof(int));
 }
 
 // unsigned intのシリアライズ
-bool CMemoryStreamWriter::Serialize(const unsigned int *pData)
+bool CMemoryStreamWriter::Serialize(unsigned int *pData)
 {
 	unsigned int Data = CEndianConverter::Convert(*pData);
 	return Write(&Data, sizeof(unsigned int));
 }
 
 // shortのシリアライズ
-bool CMemoryStreamWriter::Serialize(const short *pData)
+bool CMemoryStreamWriter::Serialize(short *pData)
 {
 	short Data = CEndianConverter::Convert(*pData);
 	return Write(&Data, sizeof(short));
 }
 
 // unsigned shortのシリアライズ
-bool CMemoryStreamWriter::Serialize(const unsigned short *pData)
+bool CMemoryStreamWriter::Serialize(unsigned short *pData)
 {
 	unsigned short Data = CEndianConverter::Convert(*pData);
 	return Write(&Data, sizeof(unsigned short));
 }
 
+// charのシリアライズ
+bool CMemoryStreamWriter::Serialize(char *pData)
+{
+	return Write(pData, sizeof(char));
+}
+
+// unsigned charのシリアライズ
+bool CMemoryStreamWriter::Serialize(unsigned char *pData)
+{
+	return Write(pData, sizeof(unsigned char));
+}
+
 // floatのシリアライズ
-bool CMemoryStreamWriter::Serialize(const float *pData)
+bool CMemoryStreamWriter::Serialize(float *pData)
 {
 	float Data = CEndianConverter::Convert(*pData);
 	return Write(&Data, sizeof(float));
 }
 
 // 文字列のシリアライズ
-bool CMemoryStreamWriter::Serialize(const char *pData)
+bool CMemoryStreamWriter::Serialize(std::string *pData)
 {
 	// 文字列長.
-	unsigned int Length = strlen(pData);
+	unsigned int Length = pData->length();
 	if (!Serialize(&Length)) { return false; }
 
 	// 文字列.
-	return Write(pData, Length);
+	return Write(pData->c_str(), Length);
 }
 
 
