d611fec5c6b42d815c6fbd758b9fa772ff354b8b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-05-21 02:39:25 +0900

    ドキュメントの更新。

--- docs/html/search/functions_b.js
+++ docs/html/search/functions_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['recv',['Recv',['../class_yana_p_server_1_1_socket_1_1_i_socket.html#ad09afe5bbf1f8d203bc3cadd33a164b8',1,'YanaPServer::Socket::ISocket::Recv()'],['../class_yana_p_server_1_1_socket_1_1_c_windows_socket.html#acaafa8caacd5f71e2bfdabec9f17c66a',1,'YanaPServer::Socket::CWindowsSocket::Recv()']]],
-  ['release',['Release',['../class_yana_p_server_1_1_socket_1_1_c_null_socket.html#aab4f6ba0b0444f08b5c750d657c4d126',1,'YanaPServer::Socket::CNullSocket::Release()'],['../class_yana_p_server_1_1_socket_1_1_c_socket.html#a881d0e1ceedb2a8310c21085edbb3908',1,'YanaPServer::Socket::CSocket::Release()']]],
+  ['recv',['Recv',['../class_yana_p_server_1_1_socket_1_1_c_null_socket.html#aa9d6c3aa8f52e1c6b7f3de2993c66781',1,'YanaPServer::Socket::CNullSocket::Recv()'],['../class_yana_p_server_1_1_socket_1_1_i_socket.html#ad09afe5bbf1f8d203bc3cadd33a164b8',1,'YanaPServer::Socket::ISocket::Recv()'],['../class_yana_p_server_1_1_socket_1_1_c_windows_socket.html#acaafa8caacd5f71e2bfdabec9f17c66a',1,'YanaPServer::Socket::CWindowsSocket::Recv()']]],
+  ['release',['Release',['../class_yana_p_server_1_1_socket_1_1_c_socket.html#a881d0e1ceedb2a8310c21085edbb3908',1,'YanaPServer::Socket::CSocket']]],
   ['removeoverrideeventlistener',['RemoveOverrideEventListener',['../class_yana_p_server_1_1_peer_1_1_c_peer_base.html#aaaa7ad919a1dc7b51f8507ce57e805ad',1,'YanaPServer::Peer::CPeerBase']]]
 ];
