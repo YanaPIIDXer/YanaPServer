4876dec88463a8e6c7202f25bb6d3ff5eb3a481a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-05-20 13:27:14 +0900

    テストサーバ・テストクライアント追加。

---
+++ TestClient/Main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include "YanaPServer/Socket/WidnowsSocket.h"
+#include "YanaPServer/Socket/SocketEventListener.h"
+
+using namespace YanaPServer::Socket;
+
+// ソケットイベント
+class SocketEvent : public ISocketEventListener
+{
+
+public:
+
+	// コンストラクタ
+	SocketEvent()
+	{
+	}
+
+	// デストラクタ
+	virtual ~SocketEvent()
+	{
+	}
+
+	// 接続された
+	virtual void OnConnect() override
+	{
+		std::cout << "Connected." << std::endl;
+	}
+
+	// 受信した。
+	virtual void OnRecv(const char * pData, unsigned int Size) override
+	{
+		std::cout << "RecvData Size:" << Size << std::endl;
+		std::cout << "Data:" << pData << std::endl;
+	}
+
+	// 切断された。
+	virtual void OnDisconnect() override
+	{
+		std::cout << "Disconnect." << std::endl;
+	}
+
+};
+
+int main()
+{
+	std::cout << "Test Client" << std::endl;
+
+	CWindowsSocket Socket;
+	SocketEvent Event;
+	Socket.SetEventListener(&Event);
+	if (!Socket.Connect("127.0.0.1", 4424)) { return 1; }
+
+	while (true)
+	{
+		Socket.Poll();
+		std::string SendData;
+		std::cout << "送信データ：";
+		std::cin >> SendData;
+		if (Socket.Send(SendData.c_str(), SendData.size() + 1))
+		{
+			std::cout << "Send Success." << std::endl;
+		}
+		else
+		{
+			std::cout << "Send Failed." << std::endl;
+			break;
+		}
+	}
+
+	std::string Dummy;
+	std::cin >> Dummy;
+	
+	return 0;
+}
---
+++ TestServer/Main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include "YanaPServer/Application/ApplicationBase.h"
+#include "YanaPServer/Peer/PeerBase.h"
+
+using namespace YanaPServer::Application;
+using namespace YanaPServer::Peer;
+using namespace YanaPServer::Socket;
+
+// Peer
+class Peer : public CPeerBase
+{
+
+public:
+
+	// コンストラクタ
+	Peer(ISocket *pSocket)
+		: CPeerBase(pSocket)
+	{
+	}
+
+	// デストラクタ
+	virtual ~Peer()
+	{
+	}
+
+	// 接続された。
+	virtual void OnConnect() override
+	{
+		std::cout << "Peer Connected." << std::endl;
+	}
+
+	// 受信した
+	virtual void OnRecv(const char * pData, unsigned int Size) override
+	{
+		std::cout << "Recv Data:" << pData << std::endl;
+
+		// そのまま送り返す。
+		Send(pData, Size);
+	}
+
+	// 切断された。
+	virtual void OnDisconnect() override
+	{
+		std::cout << "Peer Disconnected." << std::endl;
+	}
+
+private:
+
+
+};
+
+// サーバ
+class Server : public CApplicationBase
+{
+
+public:
+
+	// コンストラクタ
+	Server()
+		: CApplicationBase([this](PeerPtr pPeer)
+	{
+	})
+	{
+	}
+
+	// デストラクタ
+	virtual ~Server()
+	{
+	}
+
+protected:
+
+	// Peer生成.
+	virtual YanaPServer::Peer::CPeerBase *CreatePeer(YanaPServer::Socket::ISocket * pSocket) override
+	{
+		return new Peer(pSocket);
+	}
+
+
+private:
+
+};
+
+int main()
+{
+	std::cout << "Test Server" << std::endl;
+
+	Server Sv;
+	if (!Sv.StartListen(4424)) { return 1; }
+
+	while(Sv.Service()){}
+
+	return 0;
+}
