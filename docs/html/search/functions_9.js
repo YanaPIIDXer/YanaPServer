ce62a6961d85679508687a3b1a1719e2a9c9823b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-05-20 14:39:12 +0900

    ドキュメント再生成。

--- docs/html/search/functions_9.js
+++ docs/html/search/functions_9.js
@@ -5,5 +5,6 @@ var searchData=
   ['onerror',['OnError',['../class_yana_p_server_1_1_servlet_1_1_i_servlet.html#a27472242a99d02d151e66a84553c0088',1,'YanaPServer::Servlet::IServlet']]],
   ['onget',['OnGet',['../class_yana_p_server_1_1_servlet_1_1_i_servlet.html#ab53dcf95ccc38fdaa275edf9ce52ae39',1,'YanaPServer::Servlet::IServlet']]],
   ['onpost',['OnPost',['../class_yana_p_server_1_1_servlet_1_1_i_servlet.html#a4bf579f9e173ea1ba47df71cb48ce35f',1,'YanaPServer::Servlet::IServlet']]],
-  ['onrecv',['OnRecv',['../class_yana_p_server_1_1_servlet_1_1_c_servlet_peer.html#a6e36eac1cb2917b1437350a848b85136',1,'YanaPServer::Servlet::CServletPeer::OnRecv()'],['../class_yana_p_server_1_1_socket_1_1_i_socket_event_listener.html#ac4dbcbd61efefdddee7736ec2a35efcf',1,'YanaPServer::Socket::ISocketEventListener::OnRecv()']]]
+  ['onrecv',['OnRecv',['../class_yana_p_server_1_1_servlet_1_1_c_servlet_peer.html#a6e36eac1cb2917b1437350a848b85136',1,'YanaPServer::Servlet::CServletPeer::OnRecv()'],['../class_yana_p_server_1_1_socket_1_1_i_socket_event_listener.html#ac4dbcbd61efefdddee7736ec2a35efcf',1,'YanaPServer::Socket::ISocketEventListener::OnRecv()']]],
+  ['onsend',['OnSend',['../class_yana_p_server_1_1_servlet_1_1_c_servlet_peer.html#a6d2ed62d81197a7f5d9137e40c913508',1,'YanaPServer::Servlet::CServletPeer::OnSend()'],['../class_yana_p_server_1_1_socket_1_1_i_socket_event_listener.html#ae7a021b55c409e3630209d6b3bc2bac2',1,'YanaPServer::Socket::ISocketEventListener::OnSend()']]]
 ];
