14240206fecf9229bb03ad49f649054bb551f784
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-05-26 15:43:10 +0900

    一旦撤去。

--- docs/html/search/defines_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['socket_5ferror',['SOCKET_ERROR',['../_linux_socket_8cpp.html#a633b0396ff93d336a088412a190a5072',1,'SOCKET_ERROR():&#160;LinuxSocket.cpp'],['../_linux_listen_socket_8cpp.html#a633b0396ff93d336a088412a190a5072',1,'SOCKET_ERROR():&#160;LinuxListenSocket.cpp']]],
-  ['string_5fbuffer_5flength',['STRING_BUFFER_LENGTH',['../_my_sql_binds_8h.html#adfd4d6129d983b4e02faacbf3ee5aed6',1,'MySqlBinds.h']]]
-];
--- docs/html/search/functions_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['executequery',['ExecuteQuery',['../class_yana_p_server_1_1_my_s_q_l_1_1_c_my_sql_query.html#a471fcca1877e336e098cbe92efa084a3',1,'YanaPServer::MySQL::CMySqlQuery']]]
-];
